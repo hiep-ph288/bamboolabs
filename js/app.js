636109e0b00b3b4f855f5063959acbde36f312a8
hiepph-kimei <hiepph@kimei.vn>
2024-04-08 22:44:47 +0700

    feat: add animation provide

--- js/app.js
+++ js/app.js
@@ -2,3 +2,17 @@ window.addEventListener("scroll", () => {
   const mainNav = document.querySelector(".main-header__navbar");
   mainNav.classList.toggle("active", window.scrollY > 0);
 });
+
+const scrollExplore = document.querySelector(".scroll-explore-before");
+
+const newImageSrc = "./assets/header/scroll-explore-after.svg";
+
+const originalImageSrc = scrollExplore.getAttribute("src");
+
+scrollExplore.addEventListener("mouseover", function () {
+  scrollExplore.setAttribute("src", newImageSrc);
+});
+
+scrollExplore.addEventListener("mouseout", function () {
+  scrollExplore.setAttribute("src", originalImageSrc);
+});
